36421ee628432bb8e16639d7b8fb487ec69ecd2c
Matt Lummus <matthew.w.lummus@gmail.com>
2014-03-20 12:19:50 -0500

    end of demo session

--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -1,3 +1,5 @@
+/* global io:true */
+
 (function(){
 
   'use strict';
@@ -6,7 +8,30 @@
 
   function initialize(){
     $(document).foundation();
+    initializeSocketIO();
+    $('button').click(sendMessage);
+  }
+  var socket;
+
+  function initializeSocketIO(){
+    socket = io.connect('/app');
+    socket.on('online', function(data){console.log(data);});
+    socket.on('message', addMessage);
+  }
+
+  function sendMessage(){
+    var data = {};
+    data.text = $('textarea').val();
+    data.date = new Date();
+    socket.emit('newMessage', data);
+  }
+
+  function addMessage(data){
+    console.log('received message from node', data);
+    var $message = $('<li>');
+    var date = moment(data.date).calendar();
+    $message.text(date+':   '+data.text);
+    $('#messages').append($message);
   }
 
 })();
-
